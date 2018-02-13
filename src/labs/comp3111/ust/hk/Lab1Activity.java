224fa53673749abd427892b8dd2c7e5f38c4f8dd
Tina Lee <tleeae@connect.ust.hk>
2018-02-13 23:01:58 +0800

    implemented the min/max functionalities

--- src/labs/comp3111/ust/hk/Lab1Activity.java
+++ src/labs/comp3111/ust/hk/Lab1Activity.java
@@ -5,10 +5,16 @@ public class Lab1Activity {
 		
 		int[] arr = {1, 2 ,3 , 4, 5, 6, 7, 8, 9, 10};
 		int sum = 0;
+		int min = arr[0];
+		int max = arr[0];
+		
 		for(int i=0;i<arr.length;i++) {
 			sum += arr[i];
+			if(arr[i]>max) max = arr[i];
+			if(arr[i]<min) min = arr[i];
 		}
 		
 		System.out.println(String.format("Sum of the numbers is %d", sum));
+		System.out.println(String.format("Min = %d; Max = %d", min, max));
 	}
 }
