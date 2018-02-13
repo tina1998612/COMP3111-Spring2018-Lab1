c646642976fe3f9eb4a9ddd1fdd55aa6f8ac2fe0
Tina Lee <tleeae@connect.ust.hk>
2018-02-13 22:51:36 +0800

    add a congratulation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -3,5 +3,6 @@ package labs.comp3111.ust.hk;
 public class Lab1 {
 	public static void main(String[] args) {
 		System.out.println("Lab1: Introduction to Git and Github");
+		System.out.println("Congratulation! You learned the Git Basics");
 	}
 }
